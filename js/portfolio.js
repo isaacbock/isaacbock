c4aff40b5ac40178d6416b7805ffb5d16c87f707
Isaac Bock <isaactbock@wustl.edu>
2021-08-10 06:18:28 -0500

    Code standardization

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,48 +1,51 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+	// hide portfolio popups by default
+	$(".portfolio_popup").hide();
 
-     // hide portfolio popups by default
-     $('.portfolio_popup').hide();
+	// portfolio items open popup description on click
+	$(".hover_overlay").click(portfolioPopup);
 
-    // portfolio items open popup description on click
-    $('.hover_overlay').click(portfolioPopup);  
- 
-    // portfolio items open popup description on click
-    $('.fa-close').click(closePortfolioPopup);
+	// portfolio items open popup description on click
+	$(".fa-close").click(closePortfolioPopup);
 });
 
 function portfolioPopup(e) {
-    disableScroll();
+	disableScroll();
 	if (e.target instanceof HTMLDivElement) {
-		let popup = e.target.parentElement.getElementsByClassName("portfolio_popup")[0];
+		let popup =
+			e.target.parentElement.getElementsByClassName("portfolio_popup")[0];
 		popup.style.display = "block";
-	} 
-	else if (e.target.parentElement instanceof HTMLHeadingElement){
-		let popup = e.target.parentElement.parentElement.parentElement.getElementsByClassName("portfolio_popup")[0];
+	} else if (e.target.parentElement instanceof HTMLHeadingElement) {
+		let popup =
+			e.target.parentElement.parentElement.parentElement.getElementsByClassName(
+				"portfolio_popup"
+			)[0];
 		popup.style.display = "block";
-	}
-	else {
-		let popup = e.target.parentElement.parentElement.getElementsByClassName("portfolio_popup")[0];
+	} else {
+		let popup =
+			e.target.parentElement.parentElement.getElementsByClassName(
+				"portfolio_popup"
+			)[0];
 		popup.style.display = "block";
 	}
 }
 function closePortfolioPopup(e) {
-    enableScroll();
+	enableScroll();
 	let popup = e.target.parentElement.parentElement;
 	popup.style.display = "none";
 }
 
 // Disable scrolling via https://www.geeksforgeeks.org/how-to-disable-scrolling-temporarily-using-javascript/
-function disableScroll() { 
-    // Get the current page scroll position 
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft, 
-  
-        // if any scroll is attempted, set this to the previous value 
-        window.onscroll = function() { 
-            window.scrollTo(scrollLeft, scrollTop); 
-        }; 
-} 
-  
-function enableScroll() { 
-    window.onscroll = function() {}; 
-} 
+function disableScroll() {
+	// Get the current page scroll position
+	scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	(scrollLeft = window.pageXOffset || document.documentElement.scrollLeft),
+		// if any scroll is attempted, set this to the previous value
+		(window.onscroll = function () {
+			window.scrollTo(scrollLeft, scrollTop);
+		});
+}
+
+function enableScroll() {
+	window.onscroll = function () {};
+}
